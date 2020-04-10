cfe969e5771e27285e23333b368cc270138eade6
Leigh Halliday <leighhalliday@gmail.com>
2020-04-10 17:49:53 -0400

    use local storage state demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import torchLight from "./torch-light.svg";
+import torchDark from "./torch-dark.svg";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-function App() {
+const useLocalState = (key, defaultValue) => {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue === null ? defaultValue : JSON.parse(storedValue);
+  });
+
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.storageArea === localStorage && e.key === key) {
+        setValue(JSON.parse(e.newValue));
+      }
+    };
+    window.addEventListener("storage", listener);
+
+    return () => {
+      window.removeEventListener("storage", listener);
+    };
+  }, [key]);
+
+  const setValueInLocalStorage = (newValue) => {
+    setValue((currentValue) => {
+      const result =
+        typeof newValue === "function" ? newValue(currentValue) : newValue;
+      localStorage.setItem(key, JSON.stringify(result));
+      return result;
+    });
+  };
+
+  return [value, setValueInLocalStorage];
+};
+
+export default function App() {
+  const [username, setUsername] = useLocalState("username", "");
+  const [theme, setTheme] = useLocalState("theme", "light");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <body data-theme={theme} />
+      </Helmet>
+
+      <button
+        className="toggle-theme"
+        onClick={() => setTheme((cur) => (cur === "light" ? "dark" : "light"))}
+      >
+        <img
+          src={theme === "light" ? torchDark : torchLight}
+          alt="toggle theme"
+        />
+      </button>
+
+      <h1>{theme}</h1>
+
+      <input value={username} onChange={(e) => setUsername(e.target.value)} />
+    </HelmetProvider>
   );
 }
-
-export default App;
